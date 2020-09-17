71a1bf4638159a19f2952b0f563fec412b38791d
arnab.mukherjee@prudential.com.sg <amukherjee@decisionmanagementsolutions.com>
2020-09-17 10:43:44 +0800

    Updated Service and rules for validation

--- Promotion XOM/src/com/hp/model/request/ValidateBundle.java
+++ Promotion XOM/src/com/hp/model/request/ValidateBundle.java
@@ -1,16 +1,26 @@
 package com.hp.model.request;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import com.hp.model.Bundle;
 
 import lombok.AccessLevel;
+import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
 @FieldDefaults(level = AccessLevel.PROTECTED)
+@Data
 public class ValidateBundle {
 	String transactionId;
 	List<Bundle> productBundles;
 	
+	public List<Bundle> getProductBundles(){
+		if(Objects.isNull(productBundles)) {
+			productBundles = new ArrayList<>();
+		}
+		return productBundles;
+	}
 	
 }
